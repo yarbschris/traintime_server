fa3cb26feb01e891d85f9ce29e6220a709d491aa
Christopher Yarbro <chrisyarbro@proton.me>
2026-09-11 14:09:39 -0500

    Async endpoint determination

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 use crate::rt_data;
+use futures::future::join_all;
 use serde_norway;
 use std::collections::HashMap;
 
@@ -38,21 +39,37 @@ impl SelectedStationConfig {
         route_lookup: &HashMap<String, Vec<String>>,
     ) -> SelectedStationConfig {
         let relevant_routes = route_lookup.get(station_name).unwrap();
-        let mut relevant_endpoints = Vec::new();
-        for endpoint in &system_config.gtfs_rt_endpoints {
-            let decoded_entities = rt_data::fetch_and_decode_gtfs_rt(endpoint.clone()).await;
-            for route in rt_data::accumulate_entities_routes(decoded_entities) {
-                if relevant_routes.contains(&route) {
-                    relevant_endpoints.push(endpoint.clone());
-                    break;
-                }
-            }
-        }
+
+        let futures = system_config
+            .gtfs_rt_endpoints
+            .iter()
+            .map(|endpoint| determine_endpoint_routes(endpoint, relevant_routes));
+
+        let relevant_endpoints = join_all(futures)
+            .await
+            .drain(..)
+            .flatten()
+            .collect::<Vec<String>>();
 
         SelectedStationConfig {
             station_name: station_name.to_string(),
             relevant_endpoints,
         }
+    }
+}
+
+async fn determine_endpoint_routes(
+    endpoint: &String,
+    relevant_routes: &[String],
+) -> Option<String> {
+    let decoded_entities = rt_data::fetch_and_decode_gtfs_rt(endpoint.clone()).await;
+    if rt_data::accumulate_entities_routes(decoded_entities)
+        .iter()
+        .any(|x| relevant_routes.contains(x))
+    {
+        Some(endpoint.to_owned())
+    } else {
+        None
     }
 }
 
